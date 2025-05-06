cda81dd3707808ede93ac5a834004252fa57e444
Nathalie Himaya <natramoshi@gmail.com>
2025-05-05 23:49:34 -0700

    intial commit

---
+++ the-odin-project/myjs.js
@@ -0,0 +1,13 @@
+let name = "John";
+let surname="Doe";
+
+console.log(name); //prints
+console.log(surname);
+
+let age = 11;
+console.log(age);
+age = 54; //rewrites the age variable also variable is already declared so we dont need it  
+console.log(age);
+
+const pi = 3.4; //const cant overwrite the variable (reassign)
+console.log(pi);
